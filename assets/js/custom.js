84c28eb29609209aeaef4852e6b564ec8ba458c3
saadaf ahmed <hafijnahid45@gmail.com>
2021-01-05 23:25:51 +0600

    add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-	 new WOW().init();
+$(document).ready(function() {
+    new WOW().init();
 
-   AOS.init({
-      duration: 2000,
-     });
+    AOS.init({
+        duration: 2000,
+    });
 
-$('.header-main').ripples({
-	dropRadius:50,
-	perturbance: 0.05,
-	});
+    $('.header-main').ripples({
+        dropRadius: 20,
+        perturbance: 0.03,
+    });
 
 
 
