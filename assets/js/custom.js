38e679900b534228cb905e756fcda4c59aaf3721
saadaf ahmed <hafijnahid45@gmail.com>
2021-01-05 23:08:44 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	 new WOW().init();
+
+   AOS.init({
+      duration: 2000,
+     });
+
+$('.header-main').ripples({
+	dropRadius:50,
+	perturbance: 0.05,
+	});
+
+
+
+
+});
